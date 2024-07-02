ec581f4dc17aebb572b70ccd24e2e0ef2bb30324
ichiya <kazuya7187@gmail.com>
2024-07-02 17:11:21 +0900

    フィボナッチ数列の計算実装

---
+++ src/fib/index.ts
@@ -0,0 +1,79 @@
+import { Hono } from "hono"
+import superjson from "superjson"
+
+const fibApp = new Hono()
+
+class Fibbonacci {
+  n: number
+  memo: bigint[]
+
+  constructor(n: number) {
+    this.n = n
+    this.memo = Array.from<bigint>({ length: n + 1 }).fill(-1n)
+  }
+
+  /**
+   * フィボナッチ数列を計算する
+   * @returns 計算結果
+   */
+  calc(): bigint {
+    return this.recursive(this.n)
+  }
+
+  /**
+   * フィボナッチ数列を再帰的に計算する
+   * @param n 番号
+   * @returns 計算結果
+   */
+  private recursive(n: number): bigint {
+    if (n <= 1) {
+      return BigInt(n)
+    }
+
+    // メモ化
+    if (this.memo[n] === -1n) {
+      const result = this.recursive(n - 1) + this.recursive(n - 2)
+      this.memo[n] = result
+    }
+
+    return this.memo[n]
+  }
+}
+
+fibApp.get("/", (c) => {
+  const n = c.req.query("n")
+
+  // n が指定されていない場合
+  if (n === undefined) {
+    return c.json({
+      status: 400,
+      message: "Bad request.",
+    })
+  }
+
+  // n が数値ではない場合
+  if (!Number.isInteger(Number.parseInt(n))) {
+    return c.json({
+      status: 400,
+      message: "Bad request.",
+    })
+  }
+
+  const nInt = Number.parseInt(n)
+  // n が0以下の場合
+  if (nInt <= 0) {
+    return c.json({
+      status: 400,
+      message: "Bad request.",
+    })
+  }
+
+  // メモ化
+  const fib = new Fibbonacci(nInt)
+  const fibResult = fib.calc()
+  return c.json({
+    result: fibResult.toString(),
+  })
+})
+
+export { fibApp }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono"
+import { fibApp } from "./fib"
 
 const app = new Hono()
 
 app.get("/", (c) => {
   return c.text("Hello Hono!")
 })
+
+app.route("/fib", fibApp)
 
 export default app
